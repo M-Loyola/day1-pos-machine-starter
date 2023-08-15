8ab0b252eea0ee90a32be6558af61e00bd92d665
M-Loyola <m.yakit.loyola@gmail.com>
2023-08-15 20:42:19 +0800

    feat: add ReceiptItem class

---
+++ src/main/java/pos/machine/ReceiptItem.java
@@ -0,0 +1,34 @@
+package pos.machine;
+
+public class ReceiptItem {
+    private final String name;
+    private final int quantity;
+    private final int unitPrice;
+    private int subTotal;
+
+    public ReceiptItem(String name, int quantity, int unitPrice, int subTotal) {
+        this.name = name;
+        this.quantity = quantity;
+        this.unitPrice = unitPrice;
+        this.subTotal = subTotal;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public int getUnitPrice() {
+        return unitPrice;
+    }
+
+    public int getSubTotal() {
+        return subTotal;
+    }
+    public void calculateSubTotal() {
+        subTotal = quantity * unitPrice;
+    }
+}
